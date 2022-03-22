446baff953e1120239b2ce465b11c35f16284e4f
FERRIE Nicolas <Nicolas.FERRIE@berger-levrault.com>
2022-03-22 16:05:47 +0100

    input in progress

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Cocktail } from './interfaces/cocktail.interface';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'niceCoktail';
+  cocktail : Cocktail = {
+    name: "Mojito",
+    img : "https://static.750g.com/images/640-420/b520523117d647dab6b842a36f4cc7f5/mojito-le-vrai.jpg",
+    description : 
+    "Le mojito, prononcé [moˈxito] en espagnol, ou mojito, morito, ou mohito en français, est un cocktail traditionnel de la cuisine cubaine et de la culture de Cuba, à base de rhum, de soda, de citron vert, et de feuilles de menthe fraîche"
+  
+  }
 }
